ec249d577ec892d406c1c84d81cc6911611041b6
Belevskij Sergeij <bsa7@inbox.ru>
2013-05-11 19:23:47 +0600

    Рабочая реализация (костыль "КомандаСистемы")
    Ну вот, никто мне не помог, реализовать вызов .NET dll из 1С я не смог,
    пришлось сделать костыль - вызов exe файла с параметрами командной
    строки.
    Исходный код 1С в файле Процедура ПолнотекстовыйПоиск.txt
    В файлах Пример фильтра в справочнике товаров.png и Фильтр в справочнике
    Номенклатура-конфигуратор.png - пояснения в картинках.
    Доставляет конечно назойливое чёрное окно консоли, которое появляется
    при каждом вызове, однако существенное сокращение времени поиска радует
    сильнее. Я готов к понедельнику.

--- search.cs
+++ search.cs
@@ -15,9 +15,7 @@ namespace AddIn { //
 /*[assembly: ComVisible(true)]
 
 [assembly: Guid("d28c6322-2c2a-4763-856e-5b379cd18202")]*/
-
-
- public class SlonInDBFSearch {
+ public class InDBFSearch {
 
   public struct DBFFieldDescription {
    public string NameOfField;
@@ -34,10 +32,15 @@ namespace AddIn { //
    public ArrayList FieldsProperties;
   }
 
+
   static void Main(string[] args) {
+
    string filepath=@"SC156.DBF";
    //Пример "быстрого" выражения (в начале выражения - никаких неопределённостей по количеству символов!)
-   string pattern=@"(Н|н|H)\s*(А|а|A|a)\s*(Р|р|P|p)\s*(K|k|К|к)\s*(О|о|O|o)[^((М|м|M)(Ы|ы|O|o|О|о|У|у|Y|y|C|c|С|с))]\S*";
+   //string pattern=@"((I|i|И|и)(N|n|Н|н)(T|t|Т|т)(E|e|Е|е)(L|l|Л|л)|(A|a|А|а)(M|m|М|м)(D|d|Д|д))";
+   string pattern=@"(К|к|C|c)(О|о|O|o)(М|м|M|m)(П|п|P|p)(Ь|ь|U|u)(Ю|ю|T|t)(Т|т|E|e)";
+   string resultfilepath="result.txt";
+   //string pattern=@"(Н|н|H)\s*(А|а|A|a)\s*(Р|р|P|p)\s*(K|k|К|к)\s*(О|о|O|o)[^((М|м|M)(Ы|ы|O|o|О|о|У|у|Y|y|C|c|С|с))]\S*";
    //Пример медленных выражений. Всё дело в неопределённом заранее количестве неопределённых заранее символов (здесь 0 и более непробельных символов до начала шаблона)
    //    pattern=@"\S*?(Н|н|H)\s*(А|а|A|a)\s*(Р|р|P|p)\s*(K|k|К|к)\s*(О|о|O|o)\S*";
    //    pattern=@"\S*?(Н|н|H)\s*(А|а|A|a)\s*(Р|р|P|p)\s*(K|k|К|к)\s*(О|о|O|o)\S*";
@@ -50,38 +53,83 @@ namespace AddIn { //
     Console.WriteLine("search.exe <[Путь]ИмяФайла> <РегВыражениеПоиска>");
     Console.WriteLine("");
     Console.WriteLine("... Тем не менее - делаем тестовый прогон (наличие в текущей папке файла SC156.DBF обязательно!)");
-    Console.WriteLine("Ищем наркотики ({0}) в файле {1}", pattern, filepath);
+    //Console.WriteLine("Ищем наркотики ({0}) в файле {1}", pattern, filepath);
+    
    }
-   if (args.Length>=1) {
+   if (args.Length==3) {
     filepath=args[0];
+    //Console.WriteLine("На входе 3 параметра \n\rfilepath:|{0}|\n\rregexp:|{1}|\n\rOutput to:|{2}|", args[0], args[1], args[2]);
+    pattern=args[1];
+    resultfilepath=args[2];
    }
    if (File.Exists(filepath)) {
-    if (args.Length==2) {
-     pattern=args[1];
-    }
-    ArrayList Matches=searchInDBF(filepath, pattern);
+    //ArrayList Matches=searchInDBF(filepath, pattern);
+    searchInDBF(filepath, pattern, resultfilepath);
    }
    else { 
-    Console.WriteLine("Файл {0} не найден.", filepath);
+    //Console.WriteLine("Файл {0} не найден.", filepath);
    }
+   //System.Console.ReadKey();
   }
 
   // ************************************************************************************************************************************************
   // Функция поиска
   //
-  static ArrayList searchInDBF(string filepath, string pattern) {
+  static void searchInDBF(string filepath, string pattern, string resultfilepath) {
    string text="";
    DBFProperties DBFheader;
+   FileInfo f = new FileInfo(filepath);
+   int thisFileSize=(int)f.Length;
+   byte[] fileInBytes = new byte[thisFileSize];
    try {
-    text = System.IO.File.ReadAllText(filepath, Encoding.GetEncoding(1251)); // Тут могут возникнуть проблемы, если у тебя не Windows8 Но пока некогда разбираться с кодировками
+    //Console.WriteLine("Попытка создания FileStream.");
+    using (FileStream fs = new FileStream(filepath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite)) {
+     //Console.WriteLine("FileStream создан, попытка чтения");
+     Encoding temp = Encoding.GetEncoding("windows-1251");; //1Cv77 не работает с юникодом, 
+     //Encoding temp = new Encoding(0);
+     if (fs.Read(fileInBytes, 0, thisFileSize) == thisFileSize) {
+      //Console.WriteLine("попытка чтения успешна");
+      text=temp.GetString(fileInBytes);
+      //Console.WriteLine("{0}", text.Substring(1000,200));
+     }
+     else {
+      using (StreamWriter writer = File.CreateText("result.txt")) {
+       //Console.WriteLine("Из потока считано меньше байт, чем размер файла {0}", filepath);
+       writer.WriteLineAsync("<filename>"+filepath+"</filename>");
+       writer.WriteLineAsync("<exception>File read uncomplete.</exception>");
+      }
+      return;
+     }
+    }
    }
-   catch(IOException e) {
-    //Console.WriteLine("Обработано исключение {0}", e); Блядь, тут тоже руки не дошли. Нужен подробный разбор вариантов исключения.
-    System.IO.File.Copy(filepath, "temp.dbf", true);
+   catch {//(IOException e) {
+    //Console.WriteLine("Обработано исключение {0}", e);
+    try {
+     System.IO.File.Copy(filepath, "temp.dbf", true);
+    }
+    catch {//(IOException e1) {
+     using (StreamWriter writer = File.CreateText("result.txt")) {
+      //Console.WriteLine("Обработано исключение {0}", e1);
+      writer.WriteLineAsync("<filename>"+filepath+"</filename>");
+      writer.WriteLineAsync("<exception>FileBlocked</exception>");
+     }
+     return;
+    }
     filepath="temp.dbf";
     text = System.IO.File.ReadAllText(filepath, Encoding.GetEncoding(1251)); //В общем, надо ставить 1251, если windows 8 или что-то подходящее. Ещё надо смотреть, как БД кодирована.
    }
-   DBFheader=GetDBFRecordsCount(filepath);
+   try {
+    //Console.WriteLine("Попытка чтения заголовка");
+    DBFheader=GetDBFRecordsCount(fileInBytes);
+   }
+   catch {//(IOException e2){
+    using (StreamWriter writer = File.CreateText("result.txt")) {
+     //Console.WriteLine("Обработано исключение {0}", e2);
+     writer.WriteLineAsync("<filename>"+filepath+"</filename>");
+     writer.WriteLineAsync("<exception>Can't read file header</exception>");
+    }
+    return;
+   }
    long RecordLen=0;
    long CodeFieldStartPosition=0;
    long CodeFieldLength=0;
@@ -98,21 +146,25 @@ namespace AddIn { //
    }
    Match m = Regex.Match(text, pattern);
    ArrayList myMatches=new ArrayList(); //Только коды элементов возвращаются
-
-   while (m.Success) {
-    int inFilePosition=(int)(m.Index-DBFheader.HeaderSize)/DBFheader.RecordSize;
-    //Console.WriteLine("{0} -> {1}", CodeFieldLength, (int)CodeFieldLength);
-    string Code=text.Substring((int)(CodeFieldStartPosition+DBFheader.HeaderSize+(inFilePosition)*DBFheader.RecordSize)+1, (int)CodeFieldLength);
-    myMatches.Add(Code);
-    Console.WriteLine("'{0}' найдено на позиции {1} raw, Код: '{2}'", m.Value, inFilePosition, Code);
-    m = m.NextMatch();
-   }   
-   Console.WriteLine("Всего в БД {0} записей, каждая по {3} полей, Размер заголовка: {2}, Размер записи: {1} Нажми любую кнопку блеать!", DBFheader.RecordsCount, DBFheader.RecordSize, DBFheader.HeaderSize, DBFheader.FieldCount);
-   System.Console.ReadKey();
-   if (filepath=="temp.dbf") {
-    System.IO.File.Delete(filepath);    
+   using (StreamWriter writer = File.CreateText(resultfilepath)) {
+    writer.WriteLineAsync("<filename>"+filepath+"</filename>");
+    writer.WriteLineAsync("<regexp>"+pattern+"</regexp>");
+    while (m.Success) {
+     int inFilePosition=(int)(m.Index-DBFheader.HeaderSize)/DBFheader.RecordSize;
+     //Console.WriteLine("{0} -> {1}", CodeFieldLength, (int)CodeFieldLength);
+     string Code=text.Substring((int)(CodeFieldStartPosition+DBFheader.HeaderSize+(inFilePosition)*DBFheader.RecordSize)+1, (int)CodeFieldLength);
+     myMatches.Add(Code);
+     //Console.WriteLine("'{0}' найдено на позиции {1} raw, Код: '{2}'", m.Value, inFilePosition, Code);
+     writer.WriteLine("<code>"+Code+"</code>");
+     m = m.NextMatch();
+    }   
    }
-   return (myMatches);
+   //Console.WriteLine("Всего в БД {0} записей, каждая по {3} полей, Размер заголовка: {2}, Размер записи: {1} Нажми любую кнопку блеать!", DBFheader.RecordsCount, DBFheader.RecordSize, DBFheader.HeaderSize, DBFheader.FieldCount);
+   //System.Console.ReadKey();
+/*   if (filepath=="temp.dbf") {
+    System.IO.File.Delete(filepath);    
+   }*/
+   //return (myMatches);
   }
 
 
@@ -120,41 +172,26 @@ namespace AddIn { //
   // Функция возвращает количество записей в таблице. Чтобы избежать проблем с переполнением - возвращает long - 64 битное число
   // Так же возвращает структуру таблицы в массиве {NameOfField, TypeOfField, FieldLength}
   // 
-  static DBFProperties GetDBFRecordsCount(string filepath) {
+  static DBFProperties GetDBFRecordsCount(byte[] header) {
    const int HEADERSIZE = 32;
    DBFProperties thisDBFprops;
-   byte[] header = new byte[HEADERSIZE];
-   FileInfo f = new FileInfo(filepath);
-   thisDBFprops.FileSize=f.Length;
-   using (StreamReader reader = new StreamReader(filepath)) {
-    using (BinaryReader binaryReader = new BinaryReader(reader.BaseStream)) {
-     binaryReader.Read(header, 0, HEADERSIZE);
-     thisDBFprops.RecordsCount=header[7]*(long)Math.Pow(256, 3)+header[6]*(long)Math.Pow(256, 2)+header[5]*256+header[4];
-     thisDBFprops.HeaderSize=header[9]*256+header[8];
-     thisDBFprops.RecordSize=header[11]*256+header[10];
-    }
-   }
+   thisDBFprops.FileSize=header.Length;
+   thisDBFprops.RecordsCount=header[7]*(long)Math.Pow(256, 3)+header[6]*(long)Math.Pow(256, 2)+header[5]*256+header[4];
+   thisDBFprops.HeaderSize=header[9]*256+header[8];
+   thisDBFprops.RecordSize=header[11]*256+header[10];
    thisDBFprops.FieldCount=(int)(thisDBFprops.FileSize-thisDBFprops.RecordSize*thisDBFprops.RecordsCount-32)/32;
    thisDBFprops.FieldsProperties = new ArrayList();
-   byte[] allheader = new byte[HEADERSIZE*(1+thisDBFprops.FieldCount)];
-   using (StreamReader reader = new StreamReader(filepath)) {
-    using (BinaryReader binaryReader = new BinaryReader(reader.BaseStream)) {
-     binaryReader.Read(allheader, 0, HEADERSIZE*(1+thisDBFprops.FieldCount));
- 
-     // Немножко говнокода, к сожалению for(;;) не знаю пока как заменить на итератор
-     for (int i=1; i<=thisDBFprops.FieldCount; i++) {
-      string strFieldName="";
-      for (long j=0; j<=10; j++) {
-       if (allheader[j+i*HEADERSIZE]==0) break;
-       strFieldName+=(char)allheader[j+i*HEADERSIZE];
-      }
-      DBFFieldDescription fieldprops;
-      fieldprops.NameOfField=strFieldName;
-      fieldprops.TypeOfField=allheader[11+i*HEADERSIZE];
-      fieldprops.FieldLength=allheader[16+i*HEADERSIZE];
-      thisDBFprops.FieldsProperties.Add(fieldprops);
-     }
+   for (int i=1; i<=thisDBFprops.FieldCount; i++) {
+    string strFieldName="";
+    for (long j=0; j<=10; j++) {
+     if (header[j+i*HEADERSIZE]==0) break;
+     strFieldName+=(char)header[j+i*HEADERSIZE];
     }
+    DBFFieldDescription fieldprops;
+    fieldprops.NameOfField=strFieldName;
+    fieldprops.TypeOfField=header[11+i*HEADERSIZE];
+    fieldprops.FieldLength=header[16+i*HEADERSIZE];
+    thisDBFprops.FieldsProperties.Add(fieldprops);
    }
   return (thisDBFprops);
   }
